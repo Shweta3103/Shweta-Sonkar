1f899c5f69a382c25386d2a9a8be6f9bc5d1ebaa
Shweta3103 <101258551+Shweta3103@users.noreply.github.com>
2022-09-27 22:24:13 +0530

    Update sudo.py

--- sudo.py
+++ sudo.py
@@ -2,4 +2,14 @@
 inputfile=open('/home/user/Documents/project codes/pythonProject/sudoku_input_file.txt',mode='r')
 for lines in inputfile:
     print(lines)
+    
+outputfile=open('/home/user/Documents/project codes/pythonProject/sudoku_output_file.txt',mode='w')
+for lines in inputfile:
+    outputfile.write(lines)
+outputfile.close()
+
+def string_to_list(Str = None):
+    lisst=str.split(",")
+    return lisst
+
 
