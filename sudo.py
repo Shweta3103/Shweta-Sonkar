9c40e6bab47a219e5cd92b0b166d3f918d72a772
Shweta3103 <101258551+Shweta3103@users.noreply.github.com>
2022-09-27 13:13:16 +0530

    Update sudo.py

--- sudo.py
+++ sudo.py
@@ -2,4 +2,10 @@
 inputfile=open('/home/user/Documents/project codes/pythonProject/sudoku_input_file.txt',mode='r')
 for lines in inputfile:
     print(lines)
+    
+
+outputfile=open('/home/user/Documents/project codes/pythonProject/sudoku_output_file.txt',mode='w')
+for lines in inputfile:
+    outputfile.write(lines)
+outputfile.close()
 
