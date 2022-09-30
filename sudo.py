80811054330161d091ce754acd8c20b0ea2494fb
Shweta3103 <101258551+Shweta3103@users.noreply.github.com>
2022-09-30 12:01:14 +0530

    Update sudo.py

--- sudo.py
+++ sudo.py
@@ -8,4 +8,21 @@ outputfile=open('/home/user/Documents/project codes/pythonProject/sudoku_output_
 for lines in inputfile:
     outputfile.write(lines)
 outputfile.close()
+def string_to_list(inputfile = None):
+    result=[]
+    inputfile.seek(0, 0)
+    for line in inputfile:
+        temp = []
+        line = line.strip()
+
+        l1 = line.split(",")
+        for l in l1:
+            if l != '':
+                temp.append(int(l))
+    result.append(temp)
+    print(result)
+    return result
+   
+string_to_list(inputfile)
+
 
